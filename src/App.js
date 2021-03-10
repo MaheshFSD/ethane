2e64e68728cb03b8566292d125f72e8cdaccbb88
MaheshFSD <73214362+MaheshFSD@users.noreply.github.com>
2021-03-10 22:46:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import { Routes } from "./Routes/Routes";
-
 // import StylePage from './Components/Style/StylePage';
 import { Navbar } from "./Components/Navbar/Navbar";
-
 function App() {
   return (
     <div className="App">

1f78d6a15bb227d0b8bc95bccedde68bcebfc1df
pushpal chakrabarty <pushpalchakrabarty99@gmail.com>
2021-03-10 08:50:57 +0530

    components added

--- src/Components/Style/StylePage.jsx
+++ src/Components/Style/StylePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Footer from '../Footer/Footer'
+import EditorsPick from './EditorsPick'
+import RandomNewsBlock from './RandomNewsBlock'
+// import GetNews from './getNews'
 import StylePageCarousel from './StylePageCarousel'
 
 const StylePage = () => {
+    // GetNews()
     return (
         <div>
             <StylePageCarousel/>
+            <EditorsPick/>
+            <RandomNewsBlock/>
             <Footer/>
         </div>
     )
---
+++ src/Styles/ContainerStyles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div `
+width: 70%;
+margin: 20px auto;
+position: relative;
+`
+export const FloatingContainer = styled.div `
+position: absolute;
+padding:10px;
+top:50px;
+width:40%; 
+height:300px; 
+background-color:white;
+`
